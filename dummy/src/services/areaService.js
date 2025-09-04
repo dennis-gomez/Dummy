96fd9f8a351d739908013b8d61127f28730cfcbe
JosJos <josearmando.jimenez@ucr.ac.cr>
2025-09-04 16:28:00 -0600

    update the metod get and create the metod add in areaService

--- dummy/src/services/areaService.js
+++ dummy/src/services/areaService.js
@@ -1,15 +1,31 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/areas/";
+const API_URL = "http://localhost:3000/maintenance_areas/";
 
 // Get all areas
 export const getAreas = async () => {
     try {
         const token = localStorage.getItem("token");
-        const response = await axios(API_URL + `maintenance_areas`, {
+        const response = await axios(API_URL, {
             method: 'GET',
             headers: {
-                'Content-Type': `Barer ${token}`, 
+                'Content-Type': `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export const addArea = async (area) => {
+    try {
+        const token = localStorage.getItem("token");
+        const response = await axios.post(API_URL + `/area`, area, {
+            headers: {
+                'Content-Type': 'application/json', 
+                'Authorization': `Bearer ${token}`,
             }
         });
         return response.data;
