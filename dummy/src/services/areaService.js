c7e21b45921317ac54c332a6178294f9c373d6f5
dennis-gomez <dennis.gomez1310@gmail.com>
2025-09-04 16:08:57 -0600

    Service area creacion

---
+++ dummy/src/services/areaService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/areas/";
+
+// Get all areas
+export const getAreas = async () => {
+    try {
+        const token = localStorage.getItem("token");
+
+        const response = await axios(API_URL + ``, {
+            method: 'GET',
+            headers: {
+                'Content-Type': `Barer ${token}`, 
+            }
+        });
+
+        return response.data;
+
+    } catch (error) {
+        throw error;
+    }
+}
