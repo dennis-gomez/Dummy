6bd9c87c49977499d32211abb56a5f43b8874509
dennis-gomez <dennis.gomez1310@gmail.com>
2025-09-10 14:01:24 -0600

    Modelos catalogos (category, item y service), para realizar vista de catalogos

---
+++ dummy/src/models/service.js
@@ -0,0 +1,8 @@
+class Service {
+    constructor(cod_service, name_service) {
+        this.cod_service = cod_service;
+        this.name_service = name_service;
+    }   
+}
+
+export default Service;
