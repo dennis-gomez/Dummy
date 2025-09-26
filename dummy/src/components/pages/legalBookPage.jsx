8fac9bd86eea36d761c12b42ede49ca5d252b43c
Diyor18 <dixon.munoz@ucr.ac.cr>
2025-09-26 14:13:39 -0600

    modulo de libros legales terminado. solo falta el filtrado

---
+++ dummy/src/components/pages/legalBookPage.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import DynamicTable from '../organisms/dinamicTable';
+import { useBooks } from '../../utils/useLegalBook';
+import Form from '../organisms/form';
+import { Box, Typography } from "@mui/material";
+
+function LegalBookPage() {
+    const {
+        booksList,
+        fields,
+        searchFields,
+        loading,
+        handleAddBook,
+        handleEditBook,
+        handleDeleteBook,
+        handleSearchBooks,
+        fetchBooks, // agregamos la función fetch si quieres llamarla explícitamente
+        typesOfBooks,
+    fetchTypesOfBooks,
+    isCreatingBook,
+    setIsCreatingBook,
+    STATUS_OPTIONS,
+    
+    } = useBooks();
+
+  
+    React.useEffect(() => {
+
+console.log("campos de busqueda:", searchFields);
+
+    }, []);
+
+    return (
+        <div style={{ padding: 24 }}>
+            <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+                Gestión de Libros Legales
+            </h1>
+
+          {isCreatingBook && (
+  <Box
+    sx={{
+      maxWidth: 900,
+      margin: "20px auto",
+      p: 3,
+      boxShadow: 3,
+      borderRadius: 2,
+      backgroundColor: "#d9d9d9",
+    }}
+  >
+    <Typography variant="h6" gutterBottom color="textPrimary">
+      Agregar Nuevo Libro Legal
+    </Typography>
+
+    <Form
+  fields={fields.slice(1)} 
+  formTitle="Agregar Nuevo Libro Legal"
+  onSubmit={handleAddBook}
+  titleBtn="Guardar Libro Legal"
+/>
+  </Box>
+)}
+
+            <DynamicTable
+    fields={fields}
+    tableName="Libros Legales"
+    singularName="Libro Legal"
+    data={booksList}
+    onAdd={handleAddBook}
+    onEdit={handleEditBook}
+    onDelete={handleDeleteBook}
+    searchFields={searchFields}
+    handleSearch={handleSearchBooks}  // <-- solo esto
+    isLoading={loading}
+    textLoading="Cargando libros legales..."
+    typesOfBooks={typesOfBooks}
+    STATUS_OPTIONS={STATUS_OPTIONS}
+    isCreatingBook={isCreatingBook}
+    setIsCreatingBook={setIsCreatingBook}
+/>
+        </div>
+    );
+}
+
+export default LegalBookPage;
