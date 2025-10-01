3617d098894bbf8e12167dfd793c3725507a4ab3
JosJos <josearmando.jimenez@ucr.ac.cr>
2025-09-30 18:50:34 -0600

    small fix in validating a field

--- dummy/src/utils/useExtinguishers.js
+++ dummy/src/utils/useExtinguishers.js
@@ -45,7 +45,7 @@ const fields = [
   { name: "extinguisher_capacity_unit", placeholder: "Unidad", type: "select", width: 100, options: extinguisherCapacityUnits },
 
   { name: "extinguisher_manufacturing_date", placeholder: "Fecha de Fabricación", type: "date", width: 250, restriction: "cantAfterToday" },
-  { name: "extinguisher_last_maintenance_date", placeholder: "Último Mantenimiento", type: "date", width: 250 },
+  { name: "extinguisher_last_maintenance_date", placeholder: "Último Mantenimiento", type: "date", width: 250, restriction: "cantAfterToday" },
   { name: "extinguisher_next_date_inspection", placeholder: "Próxima Inspección", type: "date", width: 250, restriction: "cantBeforeToday" },
   { name: "extinguisher_location", placeholder: "Ubicación", type: "textarea", width: 250 },
   { name: "extinguisher_observations", placeholder: "Observaciones", type: "textarea", width: 780, required: false },
